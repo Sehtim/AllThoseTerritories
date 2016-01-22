4c5c1f0406200ad53146425b1758c55af685bba9
axr <axr@gmx.at>
2016-01-22 18:55:44 +0100

    Added Connections between territories to paint()-method, as well as the Army count and (optionally) the name of the territory to be shown. Added boolean "claimPhase" to indicate the phase of the game Added game behaviour in claim-phase, AI behaviour is still missing.

--- src/gui/WorldFrame.java
+++ src/gui/WorldFrame.java
@@ -13,6 +13,9 @@ import java.util.List;
 
 public class WorldFrame extends JFrame {
 
+    public static final int WIDTH = 1250;
+    public static final int HEIGHT = 650;
+
     private List<Continent> continents;
     private List<Territory> territories;
     private List<Player> players;
@@ -27,10 +30,21 @@ public class WorldFrame extends JFrame {
     private Territory moveToTerritory;
     private boolean movePossible;
 
+    private boolean showTerritoryNames;
+    private Font drawFont;
+
+    private boolean claimPhase;
+
     public WorldFrame(List<Player> players, List<Territory> territories, List<Continent> continents) {
         this.continents = continents;
         this.players = players;
         this.territories = territories;
+
+        this.drawFont = new Font("Arial", Font.BOLD, 12);
+        this.showTerritoryNames = false;
+
+        claimPhase = true;
+        activePlayer = 0;
 
         setTitle("AllThoseTerritories");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -42,7 +56,7 @@ public class WorldFrame extends JFrame {
                 paintGameBoard(g);
             }
         };
-        gameBoard.setSize(1250, 650);
+        gameBoard.setSize(WIDTH, HEIGHT);
         gameBoard.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
@@ -54,6 +68,29 @@ public class WorldFrame extends JFrame {
                 for (Territory territory : territories) {
                     if (territory.contains(e.getPoint())) {
                         // TODO Aktionen
+
+
+
+                        if (players.get(activePlayer).isAI())
+                        {
+                            // Evtl Meldung "AI ist an der Reihe" oder Informationen anzeigen
+                            break;
+                        }
+
+                        if (claimPhase)
+                        {
+                            if (territory.getPlayer() == -1) {
+                                territory.setPlayer(activePlayer);
+                                territory.setArmyCount(1);
+                            }
+                            else
+                            {
+                                // Evtl Meldung anzeigen: schon belegt
+                            }
+
+                            claimPhase = territories.stream().allMatch(t -> t.getPlayer() > 0);
+                        }
+
                         break;
                     }
                 }
@@ -76,6 +113,41 @@ public class WorldFrame extends JFrame {
     private void paintGameBoard(Graphics g) {
         // TODO alles zeichnen
 
+        // Verbindungen zeichnen
+        g.setColor(Color.BLACK);
+        for (int i = 0; i < territories.size(); i++)
+        {
+            Territory t1 = territories.get(i);
+            for (int j = i+1; j < territories.size(); j++)
+            {
+                Territory t2 = territories.get(j);
+                if (t1.isNeighbor(t2)) {
+                    int x1 = t1.getCapitalPosition().x;
+                    int x2 = t2.getCapitalPosition().x;
+
+                    if (Math.abs(x1 - x2) > WIDTH / 2)
+                    {
+                        int y = (t1.getCapitalPosition().y + t2.getCapitalPosition().y) / 2;
+                        if (x1 < x2)
+                        {
+                            g.drawLine(0, y, x1, t1.getCapitalPosition().y);
+                            g.drawLine(x2, t2.getCapitalPosition().y, WIDTH, y);
+                        }
+                        else
+                        {
+                            g.drawLine(0, y, x2, t2.getCapitalPosition().y);
+                            g.drawLine(x1, t1.getCapitalPosition().y, WIDTH, y);
+                        }
+                    }
+                    else {
+                        g.drawLine(t1.getCapitalPosition().x, t1.getCapitalPosition().y,
+                                   t2.getCapitalPosition().x, t2.getCapitalPosition().y);
+                    }
+                }
+            }
+        }
+
+        // Landflächen
         for (Territory territory : territories) {
             if (territory.getPlayer() == -1) {
                 g.setColor(Color.GRAY);
@@ -89,6 +161,27 @@ public class WorldFrame extends JFrame {
             for (Polygon polygon : territory.getParts()) {
                 g.drawPolygon(polygon);
             }
+        }
+
+        // Informationen (Name, Anzahl Armeen)
+        g.setColor(Color.BLACK);
+        g.setFont(drawFont);
+        FontMetrics metrics = g.getFontMetrics(drawFont);
+
+        for (Territory territory : territories)
+        {
+            Point cap = territory.getCapitalPosition();
+
+            String toDraw;
+
+            if (showTerritoryNames) {
+                toDraw = territory.getName();
+                g.drawString(toDraw, cap.x - metrics.stringWidth(toDraw) / 2, cap.y - metrics.getHeight() / 2);
+            }
+
+            toDraw = String.valueOf(territory.getArmyCount());
+            g.drawString(toDraw, cap.x - metrics.stringWidth(toDraw) / 2, cap.y + metrics.getHeight() / 2);
+
         }
     }
 }
