942e0cb1dbd3224436d6c501f6861cef56dbc39e
axr <axr@gmx.at>
2016-01-26 22:45:25 +0100

    getRelativeLuminance

--- src/gui/WorldFrame.java
+++ src/gui/WorldFrame.java
@@ -300,12 +300,13 @@ public class WorldFrame extends JFrame implements World {
         }
 
         // Informationen (Name, Anzahl Armeen)
-        g.setColor(Color.BLACK);
         g.setFont(drawFont);
         FontMetrics metrics = g.getFontMetrics(drawFont);
 
         for (Territory territory : territories) {
             Point cap = territory.getCapitalPosition();
+            Color b = players.get(territory.getPlayer()).getColor();
+
 
             String toDraw;
 
@@ -547,4 +548,15 @@ public class WorldFrame extends JFrame implements World {
         }
     }
 
+    private static double getRelativeLuminance(Color c) {
+        double r = (double) c.getRed() / 255d;
+        double g = (double) c.getGreen() / 255d;
+        double b = (double) c.getBlue() / 255d;
+
+        r = (r <= 0.03928) ? r / 12.92d : Math.pow(r + 0.055d / 1.055d, 2.4d);
+        g = (g <= 0.03928) ? g / 12.92d : Math.pow(g + 0.055d / 1.055d, 2.4d);
+        b = (b <= 0.03928) ? b / 12.92d : Math.pow(b + 0.055d / 1.055d, 2.4d);
+
+        return 0.2126d * r + 0.7152d * g + 0.0722d * b;
+    }
 }
